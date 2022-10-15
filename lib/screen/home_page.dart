448717c71a190ec5aec13d77dd48207e8d2448ad
Maruf Robin <maruf.robin@northsouth.edu>
2022-10-16 04:19:12 +0600

    Darwer added with global key

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
           drawer: Drawer(),
+          key: _scaffoldKey,
           appBar: AppBar(
             title: Text("Robin App"),
-          )),
+          ),),
     );
   }
 }
