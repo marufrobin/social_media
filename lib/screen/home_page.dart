d7c52a1b4d5666b87fa5ce8b997593f1fd4d1703
Maruf Robin <maruf.robin@northsouth.edu>
2022-10-16 00:17:44 +0600

    App bar added

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+        title: Text("Robin App"),
+      )),
+    );
+  }
+}
